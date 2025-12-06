0204b9aa8261b096b76fc10d2fbafde42e2624d9
Pankaj Kumar <kumarpankajsonu8271@gmail.com>
2025-12-07 03:47:39 +0530

    modal

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const todo = document.querySelector('#todo');
 const progress = document.querySelector('#progress');
 const done = document.querySelector('#done');
+let dragElement = null;
 
 // console.log(todo,progress,done);
 
@@ -8,14 +9,43 @@ const tasks = document.querySelectorAll('.task');
 
 tasks.forEach(task => {
     task.addEventListener("drag", (e) => {
-        console.log("dragging", e);
+        // console.log(e);
+        dragElement = task;
     })
 })
 
-progress.addEventListener("dragenter", (e) => {
-    progress.classList.add("hover-over");
-})
+function addDropOnColumn(column) {
+    column.addEventListener("dragenter", (e) => {
+        e.preventDefault();
+        column.classList.add("hover-over");
+    })
 
-progress.addEventListener("dragleave", (e) => {
-    progress.classList.remove("hover-over");
-})
+    column.addEventListener("dragleave", (e) => {
+        e.preventDefault();
+        column.classList.remove("hover-over");
+    })
+
+    column.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    })
+
+    column.addEventListener("drop", (e) => {
+        e.preventDefault();
+        // console.log("droppped" , dragElement, column);
+        column.classList.remove("hover-over");
+        column.appendChild(dragElement);
+    })
+
+}
+addDropOnColumn(todo)
+addDropOnColumn(progress)
+addDropOnColumn(done)
+
+
+// progress.addEventListener("dragenter", (e) => {
+//     progress.classList.add("hover-over");
+// })
+
+// progress.addEventListener("dragleave", (e) => {
+//     progress.classList.remove("hover-over");
+// })
