e57a64b54752926c5ab229eb226303534a073e87
Pankaj Kumar <kumarpankajsonu8271@gmail.com>
2025-12-06 22:43:10 +0530

    js file

---
+++ script.js
@@ -0,0 +1,21 @@
+const todo = document.querySelector('#todo');
+const progress = document.querySelector('#progress');
+const done = document.querySelector('#done');
+
+// console.log(todo,progress,done);
+
+const tasks = document.querySelectorAll('.task');
+
+tasks.forEach(task => {
+    task.addEventListener("drag", (e) => {
+        console.log("dragging", e);
+    })
+})
+
+progress.addEventListener("dragenter", (e) => {
+    progress.classList.add("hover-over");
+})
+
+progress.addEventListener("dragleave", (e) => {
+    progress.classList.remove("hover-over");
+})
